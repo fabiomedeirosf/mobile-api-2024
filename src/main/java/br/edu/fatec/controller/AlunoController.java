db95fe7a0af8a7c3d98c5903b96e6c5d3cbbaaf5
Fabio Faria <fabio.faria@luizalabs.com>
2024-11-04 21:00:08 -0300

    feat: add methods

--- src/main/java/br/edu/fatec/controller/AlunoController.java
+++ src/main/java/br/edu/fatec/controller/AlunoController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +22,42 @@ public class AlunoController {
 	@GetMapping(path = "/alunos")
 	public List<Aluno> getAlunos() {
 		
+			
 		return this.listaAlunos;
+	}
+	
+	
+	@GetMapping(path = "/alunos/{codigo}")
+	public ResponseEntity<?> getAlunoById(@PathVariable(name = "codigo") Integer codigo) {
+		
+		for(Aluno a : this.listaAlunos) {
+			
+			if(codigo.equals(a.getCodigo())) {
+				return ResponseEntity
+						.status(HttpStatus.OK)
+						.body(a);
+			}
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		
+	}
+	
+	@DeleteMapping(path = "/alunos/{codigo}")
+	public ResponseEntity<?> removerById(@PathVariable(name = "codigo") Integer codigo) {
+		
+		Aluno alunoRemocao = null;
+		
+		for(Aluno a : this.listaAlunos) {
+			
+			if(codigo.equals(a.getCodigo())) {
+				alunoRemocao = a;
+			}
+		}
+		
+		this.listaAlunos.remove(alunoRemocao);
+		
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
 	}
 	
 	@PostMapping(path = "/alunos")
@@ -31,7 +68,7 @@ public class AlunoController {
 			if(a.getCpf().equals(aluno.getCpf())) {
 				return ResponseEntity
 						.status(HttpStatus.CONFLICT)
-						.body(null);
+						.body("CPF existente");
 			}
 		}
 		
